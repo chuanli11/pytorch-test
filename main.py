e451ba159b2c7420d2f8aa00f37c523cc02f9b64
chuanli11 <cl.chuanli@gmail.com>
2023-02-20 18:44:47 -0800

    update main.py

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import time
 import torch
 
-list_shapes = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+list_shapes = [32, 64, 128, 256, 512, 1024, 2048, 4096]
 num_warmup = 5
 num_sample = 10
+USE_CUDA = False
+
+
+def test_fn(shape, use_cuda):
+    x = torch.randn(shape, shape)
+    y = torch.randn(shape, shape)
+    if use_cuda:
+        x = x.to("cuda")
+        y = y.to("cuda")
+    z = torch.matmul(x, y)
+
 
 for shape in list_shapes:
     for i in range(num_warmup):
-        x = torch.randn(shape, shape)
-        y = torch.randn(shape, shape)
-        x = x.to("cuda")
-        y = y.to("cuda")
-        z = torch.matmul(x, y)
+        test_fn(shape, USE_CUDA)
         torch.cuda.synchronize()
 
     total_time = 0.0
     for i in range(num_sample):
         start = time.time()
-        x = torch.randn(shape, shape)
-        y = torch.randn(shape, shape)
-        x = x.to("cuda")
-        y = y.to("cuda")
-        z = torch.matmul(x, y)
+        test_fn(shape, USE_CUDA)
         torch.cuda.synchronize()
         end = time.time()
         ms = (end - start) * 1000
