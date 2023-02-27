80dfe6738075d34de5c556ee7ee45f4e0a425002
chuanli11 <cl.chuanli@gmail.com>
2023-02-27 09:27:03 -0800

    Add main2.py

---
+++ main2.py
@@ -0,0 +1,40 @@
+import time
+import torch
+
+list_shapes = [32, 64, 128, 256, 512, 1024, 2048, 4096]
+# list_shapes = [1024]
+num_warmup = 5
+num_sample = 10
+USE_CUDA = True
+
+
+def test_fn(x, y):
+    z = torch.matmul(x, y)
+    return torch.mean(z)
+
+
+for shape in list_shapes:
+    x = torch.randn(shape, shape)
+    y = torch.randn(shape, shape)
+    if USE_CUDA:
+        x = x.to("cuda")
+        y = y.to("cuda")
+
+    for i in range(num_warmup):
+        test_fn(x, y)
+        torch.cuda.synchronize()
+
+    total_time = 0.0
+    for i in range(num_sample):
+        start = time.time()
+        res = test_fn(x, y)
+        torch.cuda.synchronize()
+        end = time.time()
+        ms = (end - start) * 1000
+        print("Inference time: {:.4f} ms mean {}".format(ms, res))
+        total_time += ms
+    print(
+        "Average Inference time for {} ({} bytes): {:.4f} ms".format(
+            shape, x.nelement() * x.element_size() * 2, (total_time) / num_sample
+        )
+    )
